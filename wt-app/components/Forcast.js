e76a783dbabd19bad03d6f55499918e6ce6e15ea
Papawin Muenpatthanakit <pearlpapawin119@gmail.com>
2022-08-27 16:55:58 +0700

    part3 challenge

--- wt-app/components/Forcast.js
+++ wt-app/components/Forcast.js
@@ -1,13 +1,32 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Forecast(props) {
     return (
-    <View>
-        <Text>{props.main}</Text>
-        <Text>{props.description}</Text>
-        <Text>{props.temp}</Text>
-        <Text>°C</Text>
+        <View style={styles.t3}>
+        <Text style={styles.t1}>{props.main}</Text>
+        <Text style={styles.t2}>{props.description}</Text>
+        <Text style={styles.t1}>{props.temp}</Text>
+        <Text style={styles.t2}>°C</Text>
     </View>
     );
 }
+
+const styles = StyleSheet.create(
+    {
+    t1: {
+        fontSize: 35 ,
+        color: 'white',
+        textAlign: 'center',
+    },
+    t2: {
+        fontSize: 30 ,
+        color: 'white',
+        textAlign: 'center',
+    },
+    t3:{
+        backgroundColor: 'rgba(0, 0, 0, .5)',
+        width: '100%',
+    }
+    }
+);
--- wt-app/components/Weather.js
+++ wt-app/components/Weather.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
 import Forecast from "./Forcast";
 
 export default function Weather(props) {
@@ -12,8 +12,8 @@ export default function Weather(props) {
     return (
         <View>
             <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
-                <Text>Zip Code</Text>
-                <Text>{props.zipCode}</Text>
+                <Text style={styles.t1}>Zip Code</Text>
+                <Text style={styles.t2}>{props.zipCode}</Text>
                 <Forecast {...forecastInfo} />
             </ImageBackground>
         </View>
@@ -24,6 +24,20 @@ const styles = StyleSheet.create({
     backdrop: {
         alignItems: 'center',
         width: '100%',
-        height: '100%'
+        height: '100%',
+    },
+    t1: {
+        fontSize: 40 ,
+        color: 'white',
+        textAlign: 'center',
+        backgroundColor: 'rgba(0, 0, 0, .5)',
+        width: '100%',
+    },
+    t2: {
+        fontSize: 45 ,
+        color: 'white',
+        textAlign: 'center',
+        backgroundColor: 'rgba(0, 0, 0, .5)',
+        width: '100%',
     },
 });
